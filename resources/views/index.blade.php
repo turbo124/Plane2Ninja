3cea59b706ad4a3e77abd1593443989246859956
David Bomba <turbo124@gmail.com>
2017-05-17 21:47:46 +1000

    tidy up UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,7 @@
 
     <div class="container-fluid">
         <div class="row text-center"><h1>Export Data</h1></div>
-        <div class="row col-md-offset-4 col-md-4" style="margin-top:20px;"><button type="button" class="btn btn-primary btn-lg btn-block" id="clients">Download Clients</button></div>
-        <div class="row col-md-offset-4 col-md-4" style="margin-top:20px;"><button type="button" class="btn btn-info btn-lg btn-block" id="products">Download Products</button></div>
-        <div class="row col-md-offset-4 col-md-4" style="margin-top:20px;"><button type="button" class="btn btn-warning btn-lg btn-block" id="invoices">Download Invoices</button></div>
-        <div class="row col-md-offset-4 col-md-4" style="margin-top:20px;"><button type="button" class="btn btn-danger btn-lg btn-block" id="quotes">Download Quotes</button></div>
+        <div class="row col-md-offset-4 col-md-4" style="margin-top:20px;"><button type="button" class="btn btn-primary btn-lg btn-block" id="clients">Download Data</button></div>
     </div>
 
 </body>
