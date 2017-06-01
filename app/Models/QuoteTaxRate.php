5efdfe783a0dad9faa2469534f22df4318b8b9ac
David Bomba <turbo124@gmail.com>
2017-06-02 08:14:01 +1000

    typo!

--- app/Models/QuoteTaxRate.php
+++ app/Models/QuoteTaxRate.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class InvoiceTaxRate extends Model
+class QuoteTaxRate extends Model
 {
     /**
      * The table associated with the model.
