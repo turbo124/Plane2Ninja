0991cffec323615478fda4ffce37020e6d6caeff
David Bomba <turbo124@gmail.com>
2018-03-14 12:32:18 +1100

    typo in class name

--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Payment extends Model
+class PaymentMethod extends Model
 {
     /**
      * The table associated with the model.
